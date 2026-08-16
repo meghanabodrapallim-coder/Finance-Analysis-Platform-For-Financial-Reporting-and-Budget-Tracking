219329c56fc8a75dfe283960d35639c5d393bde9
Meghana <meghanabodrapallim@gmail.com>
2026-08-16 11:42:03 +0530

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -3133,5 +3133,6 @@ def import_analysis():
                            monthly_labels=monthly_labels,
                            monthly_amounts=monthly_amounts)
 if __name__ == '__main__':
+    init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
